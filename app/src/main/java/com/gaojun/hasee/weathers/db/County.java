9e3654052be8b75084bc2ac4ae9759879b7329c3
gaojun1013 <2440464137@qq.com>
2017-04-10 22:03:48 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/gaojun/hasee/weathers/db/County.java
@@ -0,0 +1,50 @@
+package com.gaojun.hasee.weathers.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 高俊 on 2017/4/10.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private int cityId;
+
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+}
